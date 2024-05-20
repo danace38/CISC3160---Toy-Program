09a7bf4eca0f13627a109684b1ea281cb6d05c50
Danil Ermolin <thedanstrongny42@gmail.com>
2024-05-20 00:12:44 -0400

    Initial commit for the project

---
+++ Toy.java
@@ -0,0 +1,18 @@
+/*
+ *  CISC 3160 -- Programming Languages
+ *  Danil Ermolin
+ *  Brooklyn College, CUNY
+ *  Spring 2023
+ *  Prof. Neng-Fa Zhou
+ *  Toy Project
+ */
+
+
+ public class Toy{
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello");
+    }
+
+ }
