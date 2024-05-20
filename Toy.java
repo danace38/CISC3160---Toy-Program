7808d1791793bd3db717e3144be02c7b268726d1
Danil Ermolin <thedanstrongny42@gmail.com>
2024-05-20 17:06:01 -0400

    Third  commit

--- Toy.java
+++ Toy.java
@@ -5,6 +5,15 @@
  *  Spring 2024
  *  Prof. Neng-Fa Zhou
  *  Toy Project
+ * 
+ *  Toy Project overview: 
+ *  ------------------
+ *  The following defines a simple language, in which a program consists of assignments and each variable is assumed to be of the integer type. The interpreter for the language is written in Java. 
+ * 
+ *  The interpreter does the following: 
+ *  1. Detects syntax errors
+ *  2. Reports uninitialized variables
+ *  3. Performs assignments if there is no error and prints out the values of all the variables after all the assignments are done.
  */
 
 
@@ -16,7 +25,7 @@ public class Toy{
 
    private static Map<String, Integer> variables = new HashMap<>(); //holding variable names and integer values
    private static Set<String> initializedString = new HashSet<>(); //Storing and keeping track of initialized string variables 
-   private static final Pattern identifierPattern = Pattern.compile("[a-zA-Z_][a-zA-Z_0-9]*"); //Matching valid variable names 
+   private static final Pattern identifyPattern = Pattern.compile("[a-zA-Z_][a-zA-Z_0-9]*"); //Matching valid variable names 
 
     public static void main(String[] args) {
 
@@ -60,7 +69,7 @@ public class Toy{
          String identifier = parts[0].trim();
          String expression = parts[1].trim();
 
-         if (!identifierPattern.matcher(identifier).matches())
+         if (!identifyPattern.matcher(identifier).matches())
 
       }
    }
