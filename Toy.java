eab28d9544896bb16b4fb1cffde087287e137d2a
Danil Ermolin <thedanstrongny42@gmail.com>
2024-05-20 22:14:49 -0400

    Fourth  commit

--- Toy.java
+++ Toy.java
@@ -37,8 +37,14 @@ public class Toy{
       };
 
       for (int i = 0; i < input.length; i++){
-         System.out.println("Input " + (i+1));
-        
+         System.out.println("Input " + (i+1) + ": ");
+         System.out.println();
+         try{
+            parse(input[i]);
+            printVar();
+         }catch (Exception exc){
+            System.out.println("Error");
+         }
       }
         
         
@@ -69,10 +75,106 @@ public class Toy{
          String identifier = parts[0].trim();
          String expression = parts[1].trim();
 
-         if (!identifyPattern.matcher(identifier).matches())
-
+         if (!identifyPattern.matcher(identifier).matches()) throw new syntaxErrorExc("Invalid identifier: " + identifier);
+         int value = evalExp(expression);
+         variables.put(identifier, value);
+         initializedString.add(identifier);
       }
    }
 
+   private static int evalExp(String expression) throws syntaxErrorExc, variableExc{
+      expression = expression.replaceAll("\\s+", "");
+      if (expression.isEmpty()) throw new syntaxErrorExc("Empty");
+
+      Stack<Integer> values = new Stack<>();
+      Stack<Character> oper = new Stack<>();
+
+      for(int i = 0; i < expression.length(); i++){
+         char c = expression.charAt(i);
+
+        if(Character.isDigit(c)){
+         int num = 0;
+         while (i < expression.length() && Character.isDigit(expression.charAt(i))){
+            num = num * 10 + (expression.charAt(i) - '0');
+            i++;
+         }
+         i--;
+         values.push(num);
+        }else if (Character.isLetter(c) || c == '_'){
+         StringBuilder sb = new StringBuilder();
+         while (i < expression.length() && (Character.isLetterOrDigit(expression.charAt(i)) || expression.charAt(i) == '_')) {
+             sb.append(expression.charAt(i));
+             i++;
+         }
+         i--;
+         String identifier = sb.toString();
+         if (!initializedString.contains(identifier)) throw new variableExc("Uninitialized variable: " + identifier);
+         values.push(variables.get(identifier));
+        } else if (c == '(') {
+         oper.push(c);
+        }else if (c == ')'){
+         while (oper.peek() != '('){
+            values.push(operator(oper.pop(), values.pop(), values.pop()));
+         }
+         oper.pop();
+        }else if (c == '+' || c == '-' || c == '*' || c == '/'){
+         while (!oper.isEmpty() && precedence(c) <= precedence(oper.peek())){
+            values.push(operator(oper.pop(), values.pop(), values.pop()));
+         }
+         oper.pop();
+      }else if(c == '-' && (i == 0 || expression.charAt(i - 1) == '(')){
+         values.push(0);
+         oper.push('-');
+      }else {
+         throw new syntaxErrorExc("Invalid: charter in expression: " + c);
+      }
+   }
+
+      while (!oper.isEmpty()){
+         values.push(operator(oper.pop(), values.pop(), values.pop()));
+      }
+
+      return values.pop();
+   
+
+   }
+
+   private static int operator (char opr, int a, int b) throws syntaxErrorExc{
+      switch (opr) {
+         case '+': 
+         return (a + b);
+         case '-':
+         return (a - b);
+         case '*':
+         return (a * b);
+         case '/':
+         if (b == 0) throw new syntaxErrorExc("Invalid: division by 0");
+         return a/b;
+         default:
+         throw new syntaxErrorExc("Invalid: unknow operator " + opr);
+            
+      }
+   }
+
+   private static int precedence(char opr){
+      switch (opr) {
+         case '+':
+         case '-':
+         return 1;
+         case '*':
+         case '/':
+         return 2;
+         case '(': 
+         return 0;
+         default:
+         return -1;
+      }
+   }
+
+   private static void printVar(){
+      for (Map.Entry<String, Integer > entry: variables.entrySet()){
+         System.out.println(entry.getKey() + " = " + entry.getValue());
+      }
+   }
 
  }
